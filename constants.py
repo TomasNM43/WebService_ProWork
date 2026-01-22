9b4fa2878163f068d2b91d60f1e60bb5ad3e0802
tninan <tninan.optimus@yobelscm.biz>
2026-01-22 01:03:31 -0500

    configuracion gitiignore

---
+++ constants.py
@@ -0,0 +1,11 @@
+# Configuración de Base de Datos
+DB_USER = "system"
+DB_PASSWORD = "Ginnet$2026"
+DB_HOST = "192.168.18.8"
+DB_PORT = "1521"
+DB_SERVICE = "orcl"
+DSN = f"{DB_HOST}:{DB_PORT}/{DB_SERVICE}"
+
+# Configuración del Web Service
+WEB_SERVICE_HOST = "0.0.0.0"
+WEB_SERVICE_PORT = 10005
